2e07ae09f771858bbcd0341affc597ed202c82c7
Saya_Nakaya <saya.n@itnav.co.jp>
2025-05-28 18:56:29 +0900

    add：記事一覧ページの作成と検索バーの作成
    -記事一覧を載せるための固定ページ用php作成
    -検索バーの作成
    -どのような動作が行われているかコメントの追加

--- functions.php
+++ functions.php
@@ -176,3 +176,13 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// 投稿のアーカイブページを作成する
+function post_has_archive($args, $post_type)
+{
+    if ('post' == $post_type) {
+        $args['rewrite'] = true; // リライトを有効にする
+        $args['has_archive'] = 'articles'; // 任意のスラッグ名
+    }
+    return $args;
+}
+add_filter('register_post_type_args', 'post_has_archive', 10, 2);
--- template-parts/content.php
+++ template-parts/content.php
@@ -9,34 +9,44 @@
 
 ?>
 
+<!-- 記事の内容を表示する -->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!-- 記事のタイトルと日付を表示する部分 -->
 	<header class="entry-header">
 		<?php
+		// 記事のタイトルを表示
+		// 単一の記事ページでは大きい見出しh1、一覧ページではh2で表示
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
+		// 記事の種類が「投稿」の場合、投稿日と投稿者を表示
 		if ( 'post' === get_post_type() ) :
 			?>
+			<!-- 投稿日と投稿者を表示するための関数を呼び出している -->
 			<div class="entry-meta">
 				<?php
-				test_posted_on();
-				test_posted_by();
+				test_posted_on();  // 投稿日を表示
+				test_posted_by();  // 投稿者を表示
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php test_post_thumbnail(); ?>
+	<?php test_post_thumbnail(); // 記事のサムネイル画像を表示 ?>
 
+	<!-- 記事の本文を表示する部分 -->
 	<div class="entry-content">
 		<?php
+		// 記事の本文を表示
 		the_content(
 			sprintf(
 				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
+					//  %s の部分には記事のタイトルが入る
+					// このテキストは画面読み上げソフトでしか見えない部分
+					// 記事を読み続けるためのリンクテキストとして使われる
 					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'test' ),
 					array(
 						'span' => array(
@@ -48,6 +58,7 @@
 			)
 		);
 
+		// 記事が複数ページに分かれている場合、ページ番号のリンクを表示
 		wp_link_pages(
 			array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'test' ),
@@ -57,7 +68,10 @@
 		?>
 	</div><!-- .entry-content -->
 
+	<!-- 記事の最後の部分（カテゴリーやタグなどを表示） -->
 	<footer class="entry-footer">
+		<!-- 記事の最後の部分（カテゴリーやタグなどを表示） -->
 		<?php test_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+	<!-- 記事のIDを表示 -->
 </article><!-- #post-<?php the_ID(); ?> -->
